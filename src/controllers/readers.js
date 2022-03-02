7727d47bc32d96282b6d5f37c08e6dd7b2a5fe21
Chris Peakin <christopher.peakin@googlemail.com>
2022-03-02 13:44:49 +0000

    added CRUD tests for readers

--- src/controllers/readers.js
+++ src/controllers/readers.js
@@ -1,9 +1,8 @@
 const getDb = require('../services/db');
+const {Reader} = require('../models');
+
 
 exports.create = async (req, res) =>{
-    const db = await getDb();
-    const {name, email} = req.body;
-
-    res.sendStatus(201);
-    done();
+    const newReader = awaitReader.create(req.body);
+    res.status(201).json(newReader);
 };
