7beddcdad5d3219d6d092837d392241597e31e3d
Chris Peakin <christopher.peakin@googlemail.com>
2022-03-03 16:16:22 +0000

    added validation to book and reader models

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,8 +1,19 @@
 module.exports = (connection, DataTypes) => {
     const schema = {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            isEmail: true,
+            },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [8, 20], //password should be between 8 and 20 characters long
+        }
     };
 
     const ReaderModel = connection.define('Reader', schema);
